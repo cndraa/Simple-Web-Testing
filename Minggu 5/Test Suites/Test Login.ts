94bcc5ceadc828cc4453ac20b2e6d59951e6de0c
didimuscandra <didimuscandra@gmail.com>
2020-03-08 01:36:41 +0700

    first commit

---
+++ Minggu 5/Test Suites/Test Login.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Menguji Login dengan data yang benar dan salah</description>
+   <name>Test Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>7c328172-42a0-40c4-8599-e070746a8e19</testSuiteGuid>
+   <testCaseLink>
+      <guid>30b4f0c7-592a-4d8f-9d6e-f8b868e0bdc3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Test Login Dians dan Kopi</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>71b03285-1461-48fa-a7c3-02ec237fc44f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Test Login Dian dan Kopi</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
